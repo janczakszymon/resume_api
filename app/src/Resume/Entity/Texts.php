a1b34796cc1ecc6d0adae3f01a872d22c7179d98
szymon <s.janczak04@gmail.com>
2024-08-20 15:49:33 +0200

    added Texts entity, fixed file permissions

---
+++ app/src/Resume/Entity/Texts.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'resume')]
+final class Texts
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 2)]
+    private ?string $language = null;
+
+    #[ORM\Column(length: 254)]
+    private ?string $section = null;
+
+    #[ORM\Column(length: 254)]
+    private ?string $textPrimary = null;
+
+    #[ORM\Column(length: 254)]
+    private ?string $textSecondary = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getLanguage(): ?string
+    {
+        return $this->language;
+    }
+
+    public function setLanguage(?string $language): void
+    {
+        $this->language = $language;
+    }
+
+    public function getSection(): ?string
+    {
+        return $this->section;
+    }
+
+    public function setSection(?string $section): void
+    {
+        $this->section = $section;
+    }
+
+    public function getTextPrimary(): ?string
+    {
+        return $this->textPrimary;
+    }
+
+    public function setTextPrimary(?string $textPrimary): void
+    {
+        $this->textPrimary = $textPrimary;
+    }
+
+    public function getTextSecondary(): ?string
+    {
+        return $this->textSecondary;
+    }
+
+    public function setTextSecondary(?string $textSecondary): void
+    {
+        $this->textSecondary = $textSecondary;
+    }
+}
